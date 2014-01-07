f2998503bb99cde8cff1f07d33314ce1c3c99fe2
Esteban Gaete <zoraskiller@gmail.com>
2014-01-07 14:43:40 -0300

    Arreglado para el nuevo tipo de comunicacion

--- intermedio.cc
+++ intermedio.cc
@@ -13,31 +13,31 @@ using namespace std;
 //Define la clase para trabajar directamente con el modulo intermedio
 Define_Module( intermedio );
 
-void intermedio::handleMessage(cMessage* msg)
-{
+void intermedio::handleMessage(cMessage* msg){
     //Si el mensaje viene desde enlace
-    if (msg->arrivedOn("desde_abajo"))
+    if (msg->arrivedOn("desde_abajo")){
         //Enviar al procesador el mensaje para mandarlo a aplicacion
         processMsgFromLowerLayer(msg);
     //Sino, el mensaje viene desde arriba
-    else
+    }else{
         //Procesar el mensaje para enviar a enlace
         processMsgFromHigherLayer(msg);
+    }
 }
 
-void intermedio::processMsgFromHigherLayer(cMessage *packet)
-{
+void intermedio::processMsgFromHigherLayer(cMessage *packet){
     string packet_name = packet->getName();
 
     if(packet_name == "START"){
         //Verde de esperando confirmación
         if (ev.isGUI()){
-            getDisplayString().setTagArg("i",1,"green");
-            bubble("Esperando Confirmación");
+            getDisplayString().setTagArg("i",1,"");
+            bubble("Conectado!");
         }
-    }
-    //Negro de desconectado
-    else if(packet_name == "END"){
+        par("conectado").setBoolValue(true);
+
+    }else if(packet_name == "END"){
+        //Negro de desconectado
         getDisplayString().setTagArg("i",1,"black");
         bubble("Desconectado!");
     }
@@ -45,14 +45,14 @@ void intermedio::processMsgFromHigherLayer(cMessage *packet)
     send(packet,"hacia_abajo");
 }
 
-void intermedio::processMsgFromLowerLayer(cMessage *packet)
-{
+void intermedio::processMsgFromLowerLayer(cMessage *packet){
     string packet_name = packet->getName();
+    bool conectado = par("conectado");
 
     //Normal de activo
     if (ev.isGUI()) getDisplayString().setTagArg("i",1,"");
 
-    if((packet_name[0]=='A' && packet_name[1]=='C' && packet_name[2]=='K' && packet_name[4]=='0') || packet_name == "CONNECT"){
+    if(((packet_name[0]=='A' && packet_name[1]=='C' && packet_name[2]=='K' && packet_name[4]=='0') || packet_name == "CONNECT") && !conectado){
         if (ev.isGUI()) bubble("Conectado!");
     }else if(packet_name == "DISCONNECT"){
         //Negro de desconectado
