c9513eb7b75da4545c466c4052949cfbd8c88db5
Esteban Gaete <zoraskiller@gmail.com>
2014-01-17 11:34:08 -0300

    Nuevos metodos para jugar con separado, estructura ordenada

--- Jugador.cpp
+++ Jugador.cpp
@@ -3,39 +3,146 @@
 using namespace std;
 
 /** Constructores **/
-	Jugador::Jugador(){
-		this->puntaje = 0;
+	Jugador::Jugador(int num_jugador){
+		this->num_jugador = num_jugador;
+		this->doblar=false;
+		this->puntaje_1 = 0;
+		this->puntaje_2 = 0;
 		this->fichas = 100;
 	}
 
 /** Sets y Gets **/
-		void setNumJugador(unsigned int num_jugador){
-			this->num_jugador = num_jugador;
+	void setNumJugador(int num_jugador){
+		this->num_jugador = num_jugador;
+	}
+
+	int Jugador::getNumJugador(){ return this->num_jugador; }
+
+	void Jugador::setMano1(std::vector<Carta> Mano){
+		this->Mano_1 = Mano;
+
+		this->puntaje_1=0;
+		for (int i = 0; i < Mano_1.size(); i++)		{
+			this->puntaje_1 += this->Mano_1[i].getValor();
 		}
 
-		unsigned int getNumJugador(){ return this->num_jugador; }
+		this->num_cartas_mano_1 = this->Mano_1.size();
+	}
 
-		void Jugador::setMano(std::vector<Carta> Mano){
-			this->Mano = Mano;
+	void Jugador::setMano1(Carta Carta){
+		this->Mano_1.push_back(Carta);
+		this->puntaje_1 += Carta.getValor();
+		this->num_cartas_mano_1 += 1;
+	}
+
+	void Jugador::setMano1(int posicion, Carta Carta){
+		int valor_anterior = this->Mano_1[posicion].getValor();
+		this->Mano_1[posicion] = Carta;
+		this->puntaje_1 -= valor_anterior;
+		this->puntaje_1 += Carta.getValor();
+	}
+
+	std::vector<Carta> Jugador::getMano1(){ return this->Mano_1; }
+
+	Carta Jugador::getMano1(int posicion){ return this->Mano_1[posicion]; }
+
+	void Jugador::setMano2(std::vector<Carta> Mano){
+		this->Mano_2 = Mano;
+
+		this->puntaje_2=0;
+		for (int i = 0; i < Mano_2.size(); i++)		{
+			this->puntaje_2 += this->Mano_2[i].getValor();
 		}
 
-		void Jugador::setMano(Carta Carta){
-			this->Mano.push_back(Carta);
+		this->num_cartas_mano_2 = this->Mano_2.size();
+	}
+
+	void Jugador::setMano2(Carta Carta){
+		this->Mano_2.push_back(Carta);
+		this->puntaje_2 += Carta.getValor();
+		this->num_cartas_mano_2 += 1;
+	}
+
+	void Jugador::setMano2(int posicion, Carta Carta){
+		int valor_anterior = this->Mano_2[posicion].getValor();
+		this->Mano_2[posicion] = Carta;
+		this->puntaje_2 -= valor_anterior;
+		this->puntaje_2 += Carta.getValor();
+	}
+
+	std::vector<Carta> Jugador::getMano2(){ return this->Mano_2; }
+
+	Carta Jugador::getMano2(int posicion){ return this->Mano_2[posicion]; }
+
+	void Jugador::setSeparado(bool separado){
+		this->separado = separado;
+	}
+
+	bool Jugador::getSeparado(){ return this->separado; }
+
+	void Jugador::setNumCartasMano1(int num_cartas_mano_1){
+		this->num_cartas_mano_1 = num_cartas_mano_1;
+	}
+
+	int Jugador::getNumCartasMano1(){ return this->num_cartas_mano_1; }
+
+	void Jugador::setNumCartasMano2(int num_cartas_mano_2){
+		this->num_cartas_mano_2 = num_cartas_mano_2;
+	}
+
+	int Jugador::getNumCartasMano2(){ return this->num_cartas_mano_2; }
+
+	void Jugador::setPuntaje1(int puntaje){
+		this->puntaje_1 = puntaje;
+	}
+
+	int Jugador::getPuntaje1(){ return this->puntaje_1; }
+
+		void Jugador::setPuntaje2(int puntaje){
+		this->puntaje_2 = puntaje;
+	}
+
+	int Jugador::getPuntaje2(){ return this->puntaje_2; }
+
+	void Jugador::setFichas(int ficha){
+		this->fichas = fichas;
+	}
+
+	int Jugador::getFichas(){ return this->fichas; }
+
+/** Funcionales **/
+	int Jugador::apostar(int cantidad){
+		if(cantidad < this->fichas){
+			this->fichas -= cantidad;
+			return cantidad;
+		}else{
+			return -1;
 		}
+	}
 
-		std::vector<Carta> Jugador::getMano(){ return this->Mano; }
-		Carta Jugador::getMano(unsigned int posicion){ return this->Mano[posicion]; 
+	void Jugador::vaciarManos(){
+		//Se deben enviar al Crupier para que las
+		//ponga en el poso de cartas
+		//Luego
 
-		void Jugador::setPuntaje(unsigned int puntaje){
-			this->puntaje = puntaje;
-		}
+		this->Mano_1.clear();
+		this->puntaje_1=0;
+		this->num_cartas_mano_1=0;
 
-		unsigned int getPuntaje(){ return this->puntaje; }
+		his->Mano_2.clear();
+		this->puntaje_2=0;
+		this->num_cartas_mano_2=0;
+	}
 
-		void setFichas(unsigned int ficha){
-			this->fichas = fichas;
-		}
+	void Jugador::separar(){
 
-		unsigned int getFichas(){ return this->fichas; }
+		this->separado = true;
 
+		this->Mano_2.push_back(this->Mano_1.back());
+		this->num_cartas_mano_2 += 1;
+		this->puntaje_2 += this->Mano_1.back().getValor();
 
+		this->puntaje_1 -= this->Mano_1.back().getValor();
+		this->Mano_1.pop_back();
+		this->num_cartas_mano_1 -= 1;
+	}
--- Jugador.h
+++ Jugador.h
@@ -4,32 +4,74 @@
 class Jugador{
 	private:
 		//permite identificar que jugador es, el jugador 0 es el Crupier
-		unsigned int num_jugador;
+		int num_jugador;
 
-		//Cartas que tiene en la mano
-		std::vector<Carta> Mano;
+		//Cartas que tiene en una mano
+		std::vector<Carta> Mano_1;
 
-		//Puntaje de las cartas que tiene en lamano
-		unsigned int puntaje;
+		//Cartas en la otra mano
+		std::vector<Carta> Mano_2;
+
+		//Es verdadero si es que se esta usando la otra mano
+		bool separado;
+
+		//Numero de cartas en la mano 1
+		int num_cartas_mano_1;
+		
+		//Numero de cartas en la mano 2
+		int num_cartas_mano_2;
+
+		//Puntaje de las cartas que tiene en la mano 1
+		int puntaje_1;
+
+		//Puntaje de las cartas que tiene en la mano 2
+		int puntaje_2;
 
 		//Cantidad de fichas que tiene el jugador
-		unsigned int fichas;
+		int fichas;
 
 	public:
 		//Constructores
-		Jugador();
+		Jugador(int num_jugador);
 
 		//Sets y Gets
-		void setNumJugador(unsigned int num_jugador);
-		unsigned int getNumJugador();
-		void setMano(std::vector<Carta> Mano);
-		void setMano(Carta Carta);
-		std::vector<Carta> getMano();
-		Carta getMano(unsigned int posicion);
-		void setPuntaje(unsigned int puntaje);
-		unsigned int getPuntaje();
-		void setFichas(unsigned int fichas);
-		unsigned int getFichas();
+		void setNumJugador(int num_jugador);
+		int getNumJugador();
+
+		void setMano1(std::vector<Carta> Mano);
+		void setMano1(Carta Carta);
+		void setMano1(int posicion, Carta Carta);
+		std::vector<Carta> getMano1();
+		Carta getMano1(int posicion);
+
+		void setMano2(std::vector<Carta> Mano);
+		void setMano2(Carta Carta);
+		void setMano2(int posicion, Carta Carta);
+		std::vector<Carta> getMano2();
+		Carta getMano2(int posicion);
+
+		void setSeparado(bool separado);
+		bool getSeparado();
+
+		void setNumCartasMano1(int num_cartas_mano_1);
+		int getNumCartasMano1();
+
+		void setNumCartasMano2(int num_cartas_mano_2);
+		int getNumCartasMano2();
+
+		void setPuntaje1(int puntaje);
+		int getPuntaje1();
+
+		void setPuntaje2(int puntaje);
+		int getPuntaje2();
+
+		void setFichas(int fichas);
+		int getFichas();
+
+		//Funcionales
+		int apostar(int cantidad);
+		void vaciarManos();
+		void separar();
 };
 
 #endif /** _JUGADOR_H_ **//
